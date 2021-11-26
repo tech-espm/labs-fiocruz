cca30d37a761f9e9f80c8eaa5b8893412acb12e8
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2021-11-26 16:10:01 -0300

    Adicionando as ODS

---
+++ enums/ods.ts
@@ -0,0 +1,22 @@
+// Manter sincronizado com models/ods.ts e sql/script.sql
+enum ODS {
+	ErradicacaoPobreza = 1,
+	FomeZeroAgriculturaSustentavel = 2,
+	SaudeBemEstar = 3,
+	EducacaoQualidade = 4,
+	IgualdadeGenero = 5,
+	AguaLimpaSaneamento = 6,
+	EnergiaLimpaAcessivel = 7,
+	TrabalhoDecenteCrescimentoEconomico = 8,
+	IndustriaInovacaoInfraestrutura = 9,
+	ReducaoDesigualdades = 10,
+	CidadesComunidadesSustentaveis = 11,
+	ConsumoProducaoResponsaveis = 12,
+	AcaoContraMudancaGlobalClima = 13,
+	VidaAgua = 14,
+	VidaTerrestre = 15,
+	PazJusticaInstituicoesEficazes = 16,
+	ParceriasMeiosImplementacao = 17
+};
+
+export = ODS;
---
+++ models/ods.ts
@@ -0,0 +1,26 @@
+import ItemNomeado = require("../data/itemNomeado");
+import ListaNomeada = require("../data/listaNomeada");
+import ODS = require("../enums/ods");
+
+// Manter sincronizado com enums/ods.ts e sql/script.sql
+const ods = new ListaNomeada([
+	new ItemNomeado(ODS.ErradicacaoPobreza, "Erradicação da pobreza"),
+	new ItemNomeado(ODS.FomeZeroAgriculturaSustentavel, "Fome zero e agricultura sustentável"),
+	new ItemNomeado(ODS.SaudeBemEstar, "Saúde e bem-estar"),
+	new ItemNomeado(ODS.EducacaoQualidade, "Educação de qualidade"),
+	new ItemNomeado(ODS.IgualdadeGenero, "Igualdade de gênero"),
+	new ItemNomeado(ODS.AguaLimpaSaneamento, "Água limpa e saneamento"),
+	new ItemNomeado(ODS.EnergiaLimpaAcessivel, "Energia limpa e acessível"),
+	new ItemNomeado(ODS.TrabalhoDecenteCrescimentoEconomico, "Trabalho decente e crescimento econômico"),
+	new ItemNomeado(ODS.IndustriaInovacaoInfraestrutura, "Indústria, Inovação e infraestrutura"),
+	new ItemNomeado(ODS.ReducaoDesigualdades, "Redução das desigualdades"),
+	new ItemNomeado(ODS.CidadesComunidadesSustentaveis, "Cidades e comunidades sustentáveis"),
+	new ItemNomeado(ODS.ConsumoProducaoResponsaveis, "Consumo e produção responsáveis"),
+	new ItemNomeado(ODS.AcaoContraMudancaGlobalClima, "Ação contra a mudança global do clima"),
+	new ItemNomeado(ODS.VidaAgua, "Vida na água"),
+	new ItemNomeado(ODS.VidaTerrestre, "Vida terrestre"),
+	new ItemNomeado(ODS.PazJusticaInstituicoesEficazes, "Paz, justiça e instituições eficazes"),
+	new ItemNomeado(ODS.ParceriasMeiosImplementacao, "Parcerias e meios de implementação"),
+]);
+
+export = ods;
